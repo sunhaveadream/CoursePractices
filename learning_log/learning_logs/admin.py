f8d3c509bffdd1083b3c423aee52b48f7136ff46
sunhaveadream <394963197@qq.com>
2022-11-16 23:10:04 +0800

    2022111623

---
+++ learning_log/learning_logs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Topic,Entry
+
+# Register your models here.
+
+from .models import Topic
+
+admin.site.register(Topic)
+admin.site.register(Entry)
