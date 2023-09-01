f4a6d8277eff9c2600a52cf91e81b2ec254a5b4d
sunhaveadream <394963197@qq.com>
2023-09-02 07:59:33 +0800

    20230902django+mysql

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -83,10 +83,17 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learning_log',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
